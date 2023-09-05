d2ebd1d1e3e7abc6cff2e50044d58881e9585298
강현욱 <bug6868@naver.com>
2023-09-05 14:59:22 +0900

    예외처리 첫번째 요구사항

--- src/main/java/com/example/postlv3/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/postlv3/jwt/JwtAuthenticationFilter.java
@@ -9,12 +9,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j(topic = "로그인 및 JWT 생성")
 //authfilter,loggingfilter 대신 편리하게 사용
